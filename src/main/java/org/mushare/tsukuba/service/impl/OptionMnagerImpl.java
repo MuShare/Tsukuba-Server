5a9e2e882a5d3568628362ac2100a6304bd1978f
lowbeeWang <578957695@qq.com>
2017-05-03 15:42:08 +0900

    Add Option dao service and controller.

---
+++ src/main/java/org/mushare/tsukuba/service/impl/OptionMnagerImpl.java
@@ -0,0 +1,9 @@
+package org.mushare.tsukuba.service.impl;
+
+import org.mushare.tsukuba.service.OptionManager;
+import org.mushare.tsukuba.service.common.ManagerTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OptionMnagerImpl extends ManagerTemplate implements OptionManager{
+}
