7d4a1cfa702db57a58ef09621041560b13e29241
Meng Li <lm2343635@126.com>
2017-05-03 17:11:44 +0900

    Get selection list api.

--- src/main/java/org/mushare/tsukuba/controller/api/OptionController.java
+++ src/main/java/org/mushare/tsukuba/controller/api/OptionController.java
@@ -1,9 +1,24 @@
 package org.mushare.tsukuba.controller.api;
 
+import org.mushare.tsukuba.controller.common.ControllerTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/api/option")
-public class OptionController {
+public class OptionController extends ControllerTemplate {
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public ResponseEntity getOptionList(@RequestParam(defaultValue = "0") int rev) {
+
+        return generateOK(new HashMap<String, Object>() {{
+            put("options", null);
+        }});
+    }
+
 }
