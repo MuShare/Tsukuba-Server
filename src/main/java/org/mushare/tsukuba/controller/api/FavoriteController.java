8c3fec9789ac251930badd30b57b325f8e356472
Meng Li <lm2343635@126.com>
2017-05-18 19:10:43 +0900

    Add favorite/like api.

--- src/main/java/org/mushare/tsukuba/controller/api/FavoriteController.java
+++ src/main/java/org/mushare/tsukuba/controller/api/FavoriteController.java
@@ -1,11 +1,38 @@
 package org.mushare.tsukuba.controller.api;
 
+import org.mushare.tsukuba.bean.UserBean;
+import org.mushare.tsukuba.controller.common.ControllerTemplate;
+import org.mushare.tsukuba.controller.common.ErrorCode;
+import org.mushare.tsukuba.service.common.Result;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/api/favorite")
-public class FavoriteController {
+public class FavoriteController extends ControllerTemplate {
 
+    @RequestMapping(value = "/like", method = RequestMethod.POST)
+    public ResponseEntity favoriteMessage(@RequestParam String mid, HttpServletRequest request) {
+        UserBean userBean = auth(request);
+        if (userBean == null) {
+            return generateBadRequest(ErrorCode.ErrorToken);
+        }
+        Result result = favoriteManager.favoriteMessage(mid, userBean.getUid());
+        if (result == Result.ObjectIdError) {
+            return generateBadRequest(ErrorCode.ErrorObjecId);
+        }
+        if (result == Result.SaveInternalError) {
+            return generateBadRequest(ErrorCode.ErrorSaveFailed);
+        }
+        return generateOK(new HashMap<String, Object>() {{
+            put("success", true);
+        }});
+    }
 
 }
