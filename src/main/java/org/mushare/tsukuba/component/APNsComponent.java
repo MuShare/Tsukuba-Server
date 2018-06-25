ba769120b796ce19f7069a91b0fdf669f2f980e3
Meng Li <lm2343635@126.com>
2018-06-25 16:57:56 +0900

    User Pushy to solve the remote notification issue.

--- src/main/java/org/mushare/tsukuba/component/APNsComponent.java
+++ src/main/java/org/mushare/tsukuba/component/APNsComponent.java
@@ -1,11 +1,16 @@
 package org.mushare.tsukuba.component;
 
-
-import com.notnoop.apns.APNS;
-import com.notnoop.apns.ApnsService;
-import com.notnoop.apns.ApnsServiceBuilder;
-import com.notnoop.apns.PayloadBuilder;
+import com.turo.pushy.apns.ApnsClient;
+import com.turo.pushy.apns.ApnsClientBuilder;
+import com.turo.pushy.apns.PushNotificationResponse;
+import com.turo.pushy.apns.util.ApnsPayloadBuilder;
+import com.turo.pushy.apns.util.SimpleApnsPushNotification;
+import com.turo.pushy.apns.util.concurrent.PushNotificationFuture;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class APNsComponent {
 
@@ -28,29 +33,57 @@ public class APNsComponent {
         this.distribution = distribution;
     }
 
-    private ApnsService service = null;
+    private ApnsClient apnsClient = null;
 
-    public ApnsService getService() {
-        if (service == null) {
-            ApnsServiceBuilder builder = APNS.newService().withCert(config.rootPath + p12, password);
-            if (distribution) {
-                service = builder.withSandboxDestination().build();
-            } else {
-                service = builder.build();
+    public ApnsClient getApnsClient() {
+        if (apnsClient == null) {
+            try {
+                apnsClient = new ApnsClientBuilder()
+                        .setApnsServer(distribution ? ApnsClientBuilder.PRODUCTION_APNS_HOST : ApnsClientBuilder.DEVELOPMENT_APNS_HOST)
+                        .setClientCredentials(new File(config.rootPath + p12), password)
+                        .build();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
             }
         }
-        return service;
+        return apnsClient;
     }
 
     public void push(String deviceToken, String alertBody, String category) {
         if (deviceToken == null || deviceToken.equals("")) {
             return;
         }
-        PayloadBuilder payload = APNS.newPayload().alertBody(alertBody).sound("default");
-        if (category != null) {
-            payload.category(category);
+        final ApnsPayloadBuilder payloadBuilder = new ApnsPayloadBuilder();
+        payloadBuilder.setAlertBody(alertBody);
+        payloadBuilder.setAlertTitle("Meng Li");
+        payloadBuilder.setSound("default");
+        payloadBuilder.setCategoryName(category);
+        final SimpleApnsPushNotification pushNotification =  new SimpleApnsPushNotification(deviceToken, "org.mushare.Tsukuba-iOS", payloadBuilder.buildWithDefaultMaximumLength());
+        final PushNotificationFuture<SimpleApnsPushNotification, PushNotificationResponse<SimpleApnsPushNotification>>
+                sendNotificationFuture = getApnsClient().sendNotification(pushNotification);
+
+        try {
+            final PushNotificationResponse<SimpleApnsPushNotification> pushNotificationResponse =
+                    sendNotificationFuture.get();
+
+            if (pushNotificationResponse.isAccepted()) {
+                System.out.println("Push notification accepted by APNs gateway.");
+            } else {
+                System.out.println("Notification rejected by the APNs gateway: " +
+                        pushNotificationResponse.getRejectionReason());
+
+                if (pushNotificationResponse.getTokenInvalidationTimestamp() != null) {
+                    System.out.println("\t…and the token is invalid as of " +
+                            pushNotificationResponse.getTokenInvalidationTimestamp());
+                }
+
+            }
+        } catch (final InterruptedException | ExecutionException e) {
+            System.err.println("Failed to send push notification.");
+            e.printStackTrace();
         }
-        getService().push(deviceToken, payload.build());
+
     }
 
 }
